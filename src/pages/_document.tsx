9dbf369127aae1da7cd8dbd361af6b66f5771f86
lbgomes <muriloluiz2003@gmail.com>
2022-12-06 20:10:39 -0300

    fix: fixing google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -62,7 +62,7 @@ export default class MyDocument extends Document {
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-XXXXXXX', {
+gtag('config', 'G-5KQT6NB7DK', {
 page_path: window.location.pathname,
 });
 `,
